f9ea9058a277c6991c5830ea021f6e5900de7b09
takasea <runtakashi@hotmail.com>
2020-04-20 19:22:23 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -27,12 +27,19 @@ struct Token
 // 現在着目しているトークン
 Token *token;
 
+//入力プログラム
+char *user_input;
+
 // エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...)
+void error_at(char *loc, char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ " );
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -53,7 +60,7 @@ bool consume(char op)
 void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
@@ -62,7 +69,7 @@ void expect(char op)
 int expect_number()
 {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -112,7 +119,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
@@ -126,6 +133,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "引数の個数がただしくありません\n");
         return 1;
     }
+
+    user_input = argv[1];
 
     //トークナイズする
     token = tokenize(argv[1]);
