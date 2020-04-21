11a2bc7280ccd94f7ada03117a12865160027b91
takasea <runtakashi@hotmail.com>
2020-04-22 05:21:22 +0900

    コードジェネレータの追加

--- 9cc.c
+++ 9cc.c
@@ -202,6 +202,38 @@ Node *primary(){
     return new_node_num(expect_number());
 }
 
+// コードジェネレータ
+void gen(Node *node){
+    if(node->kind == ND_NUM){
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch(node->kind){
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("    imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("    cqo\n");
+            printf("    idiv rdi\n");
+            break;
+    }
+
+    printf("    push rax\n");
+}
+
 
 int main(int argc, char **argv)
 {
